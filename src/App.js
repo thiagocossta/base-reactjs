311013a6730bc084b521c102065650ce8fce553b
thiago costa <thiagocossta_@hotmail.com>
2019-10-31 19:20:42 -0300

    index page to init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Homepage from './pages/Homepage';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <Homepage />
+    );
+  }
+}
+export default App;
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Homepage = () => <div>welcome :)</div>;
+
+export default Homepage;
